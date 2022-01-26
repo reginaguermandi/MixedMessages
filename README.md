f70a12431a2612e5fa535810df0784b3eaff922b
Regina Guermandi <reginaguermandi@gmail.com>
2022-01-26 18:10:04 -0300

    apply randomization

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# Mixed Messages Project from Codecademy
+# Mixed Messages Project from Codecademy - Excuse Generator
 
-Every time a user runs a program, they should get a new, randomized output.To make your program truly random, the message that it outputs should be made up of at least three different pieces of data.
+Every time a user runs the program, a new and random excuse is generate, feel free to use on you day.
 
 ## Project Objectives:
 
-+ Build a message generator program using JavaScript
-+ Use Git version control
-+ Use command line
-+ Develop locally on your computer
- 
++ Build a message generator program using JavaScript.
++ To make the program truly random, the message that it outputs should be made up of at least three different pieces of data.
++ Use Git version control.
++ Use command line.
++ Develop locally on your computer. 
+
+## What I learned
+
++ Run Javascript files with Node.js.
++ Better understanding how manipulate the DOM. 
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello Fucking World')
+const button = document.querySelector("#excuse-button");
+const excuse = document.querySelector("#excuse");
+
+const excuses = {
+    intro: ["I can't come", "Forgive my obscence,", "This is going to sound crazy but,", "Get this :", "I can't go because", "I know you're going to hate me but,", "I was minding my own business and boom!", "I feel terrible but,", "I regretfully cannot attend,", "This is going to sound like and excuse but," ],
+    scapegoat: ["my nephew", "the ghost of Michael Jackson", "the Pope", "my ex", "a high school marching band", "Dan Rather", "a sad clown", "the kid from Harry Potter", "a professional cricket team", "my Tinder date"],
+    delay: ["just shit the bed.", "died in front of me.", "won't stop telling me knock knock jokes.", "is having a nervous breakdown.", "gave me syphilis.", "poured lemonade in my gas tank.", "stabbed me.", "found my box of human teeth.", "stole my shoes.", "posted my nudes on Instagram." ]
+};
+
+// Select a random index of each array and put them together
+const randomExcuse = (e) => {
+    e.preventDefault();
+    let introIndex = excuses.intro[Math.floor(Math.random()*excuses.intro.length)];
+    let escapeIndex = excuses.scapegoat[Math.floor(Math.random()*excuses.scapegoat.length)];
+    let delayIndex = excuses.delay[Math.floor(Math.random()*excuses.delay.length)];    
+        return excuse.innerHTML = `${introIndex} ${escapeIndex} ${delayIndex}`;
+};
+
+
+button.onclick = randomExcuse;
